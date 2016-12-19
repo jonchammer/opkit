3c38f861a0eadbb6a674245659dd83fae90aae51
Jon C. Hammer <jonhammer_2008@yahoo.com>
2016-12-19 10:22:17 -0600

    SimpleGradientDescent class added for when momentum is not needed. This is much faster (at least for stochastic SGD) than the original version that includes momentum. A similar approach has been taken with RMSProp.
    RMSProp has also been accelerated, but it's still considerably slower than
    either of the GradientDescent classes.

--- include/Trainers/GradientDescent.h
+++ include/Trainers/GradientDescent.h
@@ -23,10 +23,50 @@ namespace opkit
 // by using this class with a BatchIterator, both minibatches and stochastic
 // sampling can be performed.
 //
+// NOTE: The simple gradient descender has no advanced features like Nesterov
+// momentum. Instead, it is designed to be fast.
+template <class T, class Model>
+class SimpleGradientDescent : public Trainer<T, Model>
+{
+public:
+    const T DEFAULT_LEARNING_RATE = 1E-4;
+
+    SimpleGradientDescent(ErrorFunction<T, Model>* function) :
+        Trainer<T, Model>(function),
+        mLearningRate(DEFAULT_LEARNING_RATE) {}
+
+    void iterate(const Matrix<T>& features, const Matrix<T>& labels)
+    {
+        T* params      = Trainer<T, Model>::function->getParameters().data();
+        const size_t N = Trainer<T, Model>::function->getNumParameters();
+
+        // Estimate the complete gradient
+        static vector<T> gradient(N);
+        Trainer<T, Model>::function->calculateGradientParameters(features, labels, gradient);
+
+        // Descend the gradient using accelerated vector operations
+        vAdd(gradient.data(), params, N, -mLearningRate);
+    }
+
+    // Setters / Getters
+    void setLearningRate(const T learningRate) { mLearningRate = learningRate; }
+    T getLearningRate() const                  { return mLearningRate;         }
+
+private:
+    T mLearningRate;
+};
+
+// This class implements a traditional gradient descender. It looks at every
+// sample in the given matrices in order to calculate the best possible estimate
+// of the gradient of the error surface before taking a single step. Note that
+// by using this class with a BatchIterator, both minibatches and stochastic
+// sampling can be performed.
+//
 // NOTE: This implementation also allows the use of Nesterov momentum in order
 // to improve the convergence rate. Values should be small (e.g. very close to
 // 0.0) to be effective. When the momentum value is set to 0.0, no momentum will
-// be used.
+// be used. This implementation will very likely be slightly slower than that
+// used in SimpleGradientDescent.
 template <class T, class Model>
 class GradientDescent : public Trainer<T, Model>
 {
--- include/Trainers/RMSProp.h
+++ include/Trainers/RMSProp.h
@@ -13,10 +13,83 @@
 #include "Trainer.h"
 #include "ErrorFunction.h"
 #include "Matrix.h"
+#include "Acceleration.h"
 using std::vector;
 
 namespace opkit
 {
+
+// An implementation of batch RMS prop alone. Two parameters are used to adjust
+// the performance:
+//   1 - Learning Rate - same as for traditional gradient descent.
+//   2 - Decay - Dictates size of moving window. When decay = 1.0, this
+//       implementation reduces to traditional gradient descent. Value should be
+//       between [0.0 and 1.0].
+//
+// See: http://climin.readthedocs.io/en/latest/rmsprop.html
+//      http://sebastianruder.com/optimizing-gradient-descent/index.html
+template <class T, class Model>
+class SimpleRMSProp : public Trainer<T, Model>
+{
+public:
+    const T DEFAULT_DECAY         = 0.90;
+    const T DEFAULT_LEARNING_RATE = 1E-4;
+
+    SimpleRMSProp(ErrorFunction<T, Model>* function) :
+        Trainer<T, Model>(function),
+        mRMS(function->getNumParameters(), 1.0),
+        mDecay(DEFAULT_DECAY),
+        mLearningRate(DEFAULT_LEARNING_RATE) {}
+
+    void iterate(const Matrix<T>& features, const Matrix<T>& labels)
+    {
+        T* params      = Trainer<T, Model>::function->getParameters().data();
+        T* RMS         = mRMS.data();
+        const size_t N = mRMS.size();
+
+        static vector<T> gradient(N);
+        Trainer<T, Model>::function->calculateGradientParameters(features, labels, gradient);
+
+        for (size_t i = 0; i < N; ++i)
+        {
+            // Logically, the RMS update is:
+            // RMS[i] = (1.0 - mDecay) * gradient^2 + mDecay + RMS[i]
+            // This is a reorganization of the same formula that has fewer operations.
+            T gradSquare = gradient[i] * gradient[i];
+            RMS[i]       = gradSquare + mDecay * (RMS[i] - gradSquare);
+
+            // Descend the gradient
+            // This operation is too complex to be performed using the accelerated
+            // vector operations, so we just use the simple approach.
+            params[i] -= (mLearningRate / std::sqrt(RMS[i] + 1E-8)) * gradient[i];
+        }
+    }
+
+    void setDecay(const T decay)
+    {
+        mDecay = decay;
+    }
+
+    void setLearningRate(const T learningRate)
+    {
+        mLearningRate = learningRate;
+    }
+
+    T getDecay() const
+    {
+        return mDecay;
+    }
+
+    T getLearningRate() const
+    {
+        return mLearningRate;
+    }
+
+private:
+    vector<T> mRMS;
+    T mDecay;
+    T mLearningRate;
+};
 
 // An implementation of batch RMS prop that includes Nesterov momentum. Three
 // parameters are used to adjust the performance:
@@ -49,21 +122,32 @@ public:
 
     void iterate(const Matrix<T>& features, const Matrix<T>& labels)
     {
-        vector<T>& params = Trainer<T, Model>::function->getParameters();
-        const size_t N    = params.size();
+        T* params      = Trainer<T, Model>::function->getParameters().data();
+        T* velocity    = mVelocity.data();
+        T* RMS         = mRMS.data();
+        const size_t N = mVelocity.size();
 
-        for (size_t i = 0; i < N; ++i)
-            params[i] -= mMomentum * mVelocity[i];
+        // First step for Nesterov mMomentum
+        // params -= momentum * velocity
+        vAdd(velocity, params, N, -mMomentum);
 
         static vector<T> gradient(N);
         Trainer<T, Model>::function->calculateGradientParameters(features, labels, gradient);
 
         for (size_t i = 0; i < N; ++i)
         {
-            mRMS[i]      = (1.0 - mDecay) * gradient[i] * gradient[i] + mDecay * mRMS[i];
-            T oldV       = mVelocity[i];
-            mVelocity[i] = mMomentum * oldV + (mLearningRate / std::sqrt(mRMS[i] + 1E-8) * gradient[i]);
-            params[i]    = params[i] + (mMomentum * oldV) - mVelocity[i];
+            // Logically, the RMS update is:
+            // RMS[i] = (1.0 - mDecay) * gradient^2 + mDecay + RMS[i]
+            // This is a reorganization of the same formula that has fewer operations.
+            T gradSquare = gradient[i] * gradient[i];
+            RMS[i]       = gradSquare + mDecay * (RMS[i] - gradSquare);
+
+            // Descend the gradient (and apply momentum).
+            // This operation is too complex to be performed using the accelerated
+            // vector operations, so we just use the simple approach.
+            T temp      = mMomentum * velocity[i];
+            velocity[i] = temp + (mLearningRate / (std::sqrt(RMS[i] + 1E-8)) * gradient[i]);
+            params[i]  += temp - velocity[i];
         }
     }
 
