5226a1778bb56fc7ffa44076d19cab32339d3cd9
Jon Hammer <jhammer@uark.edu>
2017-02-28 15:36:45 -0600

    Added L2 Regularizer.

--- include/VectorOps.h
+++ include/VectorOps.h
@@ -5,7 +5,32 @@
 using std::vector;
 
 template <class T>
-T magnitude(vector<T>& vec)
+T mean(const vector<T>& vec)
+{
+    T sum{};
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        sum += vec[i];
+    }
+    return sum / vec.size();
+}
+
+template <class T>
+T variance(const vector<T>& vec)
+{
+    T avg = mean(vec);
+    T sum{};
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        T temp = vec[i] - avg;
+        sum   += temp * temp;
+    }
+
+    return sum / vec.size();
+}
+
+template <class T>
+T magnitude(const vector<T>& vec)
 {
     T mag = T{};
     for (size_t i = 0; i < vec.size(); ++i)
@@ -16,7 +41,7 @@ T magnitude(vector<T>& vec)
 }
 
 template <class T>
-void normalize(vector<T>& vec)
+void normalize(const vector<T>& vec)
 {
     T mag = magnitude(vec);
     for (size_t i = 0; i < vec.size(); ++i)
