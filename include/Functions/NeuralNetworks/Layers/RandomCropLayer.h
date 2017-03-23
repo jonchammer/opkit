d51cc665d48ba3be871199afd6423dec365b96cf
Jon C. Hammer <jonhammer_2008@yahoo.com>
2017-03-23 13:41:42 -0500

    Most layer implementations have now been updated.

--- include/Functions/NeuralNetworks/Layers/RandomCropLayer.h
+++ include/Functions/NeuralNetworks/Layers/RandomCropLayer.h
@@ -13,133 +13,136 @@ namespace opkit
 // used during testing. This layer has no learned parameters, so it will
 // not contribute to the gradient, but it should preceed all learning layers
 // in the neural network.
-// template <class T>
-// class RandomCropLayer : public Layer<T>
-// {
-// public:
-//
-//     // Allows us to use the members in the base class without specifying
-//     // their complete names
-//     using Layer<T>::mActivation;
-//     using Layer<T>::mBatchSize;
-//
-//     RandomCropLayer(const size_t inputWidth, const size_t inputHeight,
-//         const size_t channels,
-//         const size_t croppedWidth, const size_t croppedHeight,
-//         const size_t batchSize,
-//         const size_t randSeed = Rand::getDefaultSeed()) :
-//
-//         Layer<T>(inputWidth * inputHeight * channels,
-//             croppedWidth * croppedHeight * channels, batchSize),
-//         mInputWidth(inputWidth),
-//         mInputHeight(inputHeight),
-//         mChannels(channels),
-//         mOutputWidth(croppedWidth),
-//         mOutputHeight(croppedHeight),
-//         mRand(randSeed),
-//         mTesting(false)
-//     {
-//         assert(mOutputWidth <= mInputWidth);
-//         assert(mOutputHeight <= mInputHeight);
-//     }
-//
-// private:
-//     void evalSingle(const Matrix<T>& x, const size_t row, const size_t cropX, const size_t cropY)
-//     {
-//         T* dest = mActivation(row);
-//
-//         for (size_t channel = 0; channel < mChannels; ++channel)
-//         {
-//             // Start at the given row in the dataset. Add a channel
-//             // offset. Then, navigate to the correct starting row and
-//             // column.
-//             const T* src = x(row) + (channel * mInputWidth * mInputHeight) +
-//                 (cropY * mInputWidth) + cropX;
-//
-//             for (size_t r = 0; r < mOutputHeight; ++r)
-//             {
-//                 vCopy(src, dest, mOutputWidth);
-//
-//                 src  += mInputWidth;
-//                 dest += mOutputWidth;
-//             }
-//         }
-//     }
-//
-// public:
-//     void eval(const Matrix<T>& x) override
-//     {
-//         if (!mTesting)
-//         {
-//             // Each sample has a different random crop
-//             for (size_t i = 0; i < mBatchSize; ++i)
-//             {
-//                 // Choose the random crop section
-//                 size_t cropX = mRand.nextInteger<size_t>(0, mInputWidth - mOutputWidth);
-//                 size_t cropY = mRand.nextInteger<size_t>(0, mInputHeight - mOutputHeight);
-//
-//                 evalSingle(x, i, cropX, cropY);
-//             }
-//         }
-//
-//         else
-//         {
-//             // Use the center of the image for testing
-//             size_t cropX = (mInputWidth - mOutputWidth)   / 2;
-//             size_t cropY = (mInputHeight - mOutputHeight) / 2;
-//
-//             for (size_t i = 0; i < mBatchSize; ++i)
-//             {
-//                 evalSingle(x, i, cropX, cropY);
-//             }
-//         }
-//     }
-//
-//     void calculateDeltas(const Matrix<T>& x, T* destination) override
-//     {
-//         // TODO: Implement. Only those cells in the previously cropped
-//         // region should be forwarded. All other cells should be 0.
-//     }
-//
-//     void calculateGradient(const Matrix<T>& x, T* gradient) override
-//     {
-//         // Do nothing. There is no gradient to calculate since there are
-//         // no optimizable parameters.
-//     }
-//
-//     size_t getNumParameters() const override
-//     {
-//         return 0;
-//     }
-//
-//     std::string getName() const override
-//     {
-//         return "Random Crop Layer";
-//     }
-//
-//     // std::string getMiscString() const override
-//     // {
-//     //     char buffer[1024];
-//     //     snprintf(buffer, 1024, "(%zux%zux%zu) -> (%zux%zux%zu)",
-//     //         mInputWidth, mInputHeight, mChannels,
-//     //         mOutputWidth, mOutputHeight, mChannels);
-//     //
-//     //     return string(buffer);
-//     // }
-//
-//     void setTesting(bool testing)
-//     {
-//         mTesting = testing;
-//     }
-//
-// private:
-//     size_t mInputWidth, mInputHeight;
-//     size_t mChannels;
-//     size_t mOutputWidth, mOutputHeight;
-//
-//     Rand mRand;
-//     bool mTesting;
-// };
+template <class T>
+class RandomCropLayer : public Layer<T>
+{
+public:
+
+    RandomCropLayer(const size_t inputWidth, const size_t inputHeight,
+        const size_t channels,
+        const size_t croppedWidth, const size_t croppedHeight,
+        const size_t randSeed = Rand::getDefaultSeed()) :
+
+        Layer<T>(inputWidth * inputHeight * channels,
+            croppedWidth * croppedHeight * channels),
+        mInputWidth(inputWidth),
+        mInputHeight(inputHeight),
+        mChannels(channels),
+        mOutputWidth(croppedWidth),
+        mOutputHeight(croppedHeight),
+        mRand(randSeed),
+        mTesting(false)
+    {
+        assert(mOutputWidth <= mInputWidth);
+        assert(mOutputHeight <= mInputHeight);
+    }
+
+    void forwardSingle(const T* x, T* y) override
+    {
+        if (!mTesting)
+        {
+            // Choose the random crop section
+            size_t cropX = mRand.nextInteger<size_t>(0, mInputWidth - mOutputWidth);
+            size_t cropY = mRand.nextInteger<size_t>(0, mInputHeight - mOutputHeight);
+
+            evalSingle(x, cropX, cropY, y);
+        }
+
+        else
+        {
+            // Use the center of the image for testing
+            size_t cropX = (mInputWidth - mOutputWidth)   / 2;
+            size_t cropY = (mInputHeight - mOutputHeight) / 2;
+
+            evalSingle(x, cropX, cropY, y);
+        }
+    }
+
+    void forwardBatch(const Matrix<T>& x, Matrix<T>& y) override
+    {
+        if (!mTesting)
+        {
+            // Each sample has a different random crop
+            for (size_t i = 0; i < x.getRows(); ++i)
+            {
+                // Choose the random crop section
+                size_t cropX = mRand.nextInteger<size_t>(0, mInputWidth - mOutputWidth);
+                size_t cropY = mRand.nextInteger<size_t>(0, mInputHeight - mOutputHeight);
+
+                evalSingle(x(i), cropX, cropY, y(i));
+            }
+        }
+
+        else
+        {
+            // Use the center of the image for testing
+            size_t cropX = (mInputWidth - mOutputWidth)   / 2;
+            size_t cropY = (mInputHeight - mOutputHeight) / 2;
+
+            for (size_t i = 0; i < x.getRows(); ++i)
+            {
+                evalSingle(x(i), cropX, cropY, y(i));
+            }
+        }
+    }
+
+private:
+    void evalSingle(const T* x, const size_t cropX, const size_t cropY, T* y)
+    {
+        T* dest = y;
+
+        for (size_t channel = 0; channel < mChannels; ++channel)
+        {
+            // Start at the given row in the dataset. Add a channel
+            // offset. Then, navigate to the correct starting row and
+            // column.
+            const T* src = x + (channel * mInputWidth * mInputHeight) +
+                (cropY * mInputWidth) + cropX;
+
+            for (size_t r = 0; r < mOutputHeight; ++r)
+            {
+                vCopy(src, dest, mOutputWidth);
+
+                src  += mInputWidth;
+                dest += mOutputWidth;
+            }
+        }
+    }
+
+public:
+
+    std::string getName() const override
+    {
+        return "Random Crop Layer";
+    }
+
+    std::string* getProperties(size_t& numElements) const override
+    {
+        std::string* arr = new std::string[1];
+
+        char buffer[1024];
+        snprintf(buffer, 1024, "(%zux%zux%zu) -> (%zux%zux%zu)",
+            mInputWidth, mInputHeight, mChannels,
+            mOutputWidth, mOutputHeight, mChannels);
+        arr[0] = string(buffer);
+
+        numElements = 1;
+        return arr;
+    }
+
+    void setTesting(bool testing)
+    {
+        mTesting = testing;
+    }
+
+private:
+    size_t mInputWidth, mInputHeight;
+    size_t mChannels;
+    size_t mOutputWidth, mOutputHeight;
+
+    Rand mRand;
+    bool mTesting;
+};
 
 }
 #endif
--- include/Functions/NeuralNetworks/Layers/RandomRotationLayer.h
+++ include/Functions/NeuralNetworks/Layers/RandomRotationLayer.h
@@ -13,146 +13,150 @@ namespace opkit
 // rotation amount is used during training, and no rotation is used duing
 // testing. This layer has no learned parameters, so it will not contribute to
 // the gradient, but it should preceed all learning layers in the neural network.
-// template <class T>
-// class RandomRotationLayer : public Layer<T>
-// {
-// public:
-//
-//     // Allows us to use the members in the base class without specifying
-//     // their complete names
-//     using Layer<T>::mActivation;
-//     using Layer<T>::mBatchSize;
-//
-//     // Create a new Random Rotation Layer. We need to know the dimensions of the
-//     // input/output, as well as maximum and minimum rotation angles.
-//     RandomRotationLayer(const size_t width, const size_t height,
-//         const size_t channels,
-//         const double minAngleDegrees, const double maxAngleDegrees,
-//         const size_t batchSize, const size_t seed = Rand::getDefaultSeed()) :
-//
-//         Layer<T>(width * height * channels,
-//             width * height * channels, batchSize),
-//         mWidth(width),
-//         mHeight(height),
-//         mChannels(channels),
-//         mMinAngle(minAngleDegrees * M_PI / 180.0),
-//         mMaxAngle(maxAngleDegrees * M_PI / 180.0),
-//         mRand(seed),
-//         mTesting(false)
-//     {
-//         // Do nothing
-//     }
-//
-// private:
-//
-//     // Rotates a single image (one row in x) by the given angle in radians.
-//     void evalSingle(const Matrix<T>& x, const size_t row, const double angle)
-//     {
-//         const T* src = x(row);
-//         T* dest      = mActivation(row);
-//
-//         double cosAngle = std::cos(-angle);
-//         double sinAngle = std::sin(-angle);
-//
-//         for (size_t channel = 0; channel < mChannels; ++channel)
-//         {
-//             for (size_t y = 0; y < mHeight; ++y)
-//             {
-//                 double ySinAngle = y * sinAngle;
-//                 double yCosAngle = y * cosAngle;
-//
-//                 for (size_t x = 0; x < mWidth; ++x)
-//                 {
-//                     // Rotate (x,y) by -angle
-//                     double xp = x * cosAngle - ySinAngle;
-//                     double yp = x * sinAngle + yCosAngle;
-//
-//                     // Interpolate via nearest neighbor
-//                     int srcX  = std::round(xp);
-//                     int srcY  = std::round(yp);
-//
-//                     // Copy the appropriate pixel
-//                     if (srcX < 0 || srcX >= mWidth || srcY < 0 || srcY >= mHeight)
-//                         *dest++ = T{};
-//                     else *dest++ = src[srcY * mWidth + srcX];
-//                 }
-//             }
-//
-//             // Advance to the next chanel
-//             src += mWidth * mHeight;
-//         }
-//     }
-//
-// public:
-//     void eval(const Matrix<T>& x) override
-//     {
-//         if (!mTesting)
-//         {
-//             // Each sample has a different rotation
-//             for (size_t i = 0; i < mBatchSize; ++i)
-//             {
-//                 // Choose the angle randomly
-//                 double angle = mRand.nextReal(mMinAngle, mMaxAngle);
-//                 evalSingle(x, i, angle);
-//             }
-//         }
-//
-//         else
-//         {
-//             // Use no rotation for testing. Just pass the data through unaltered.
-//             vCopy(x.data(), mActivation.data(), mBatchSize * x.getCols());
-//         }
-//     }
-//
-//     void calculateDeltas(const Matrix<T>& x, T* destination) override
-//     {
-//         // TODO: Implement.
-//     }
-//
-//     void calculateGradient(const Matrix<T>& x, T* gradient) override
-//     {
-//         // Do nothing. There is no gradient to calculate since there are
-//         // no optimizable parameters.
-//     }
-//
-//     size_t getNumParameters() const override
-//     {
-//         return 0;
-//     }
-//
-//     std::string getName() const override
-//     {
-//         return "Random Rotation Layer";
-//     }
-//
-//     // std::string getMiscString() const override
-//     // {
-//     //     char buffer[1024];
-//     //     snprintf(buffer, 1024, "Shape: (%zux%zux%zu), Angle Range: [%.2f, %.2f]",
-//     //         mWidth, mHeight, mChannels, getMinAngle(), getMaxAngle());
-//     //
-//     //     return string(buffer);
-//     // }
-//
-//     void setTesting(bool testing)
-//     {
-//         mTesting = testing;
-//     }
-//
-//     // Simple getters
-//     size_t getWidth()    const { return mWidth;                   }
-//     size_t getHeight()   const { return mHeight;                  }
-//     size_t getChannels() const { return mChannels;                }
-//     double getMinAngle() const { return mMinAngle * 180.0 / M_PI; }
-//     double getMaxAngle() const { return mMaxAngle * 180.0 / M_PI; }
-//
-// private:
-//     size_t mWidth, mHeight, mChannels;
-//     double mMinAngle, mMaxAngle;
-//
-//     Rand mRand;
-//     bool mTesting;
-// };
+template <class T>
+class RandomRotationLayer : public Layer<T>
+{
+
+public:
+    // Create a new Random Rotation Layer. We need to know the dimensions of the
+    // input/output, as well as maximum and minimum rotation angles.
+    RandomRotationLayer(const size_t width, const size_t height,
+        const size_t channels,
+        const double minAngleDegrees, const double maxAngleDegrees,
+        const size_t seed = Rand::getDefaultSeed()) :
+
+        Layer<T>(width * height * channels,
+            width * height * channels),
+        mWidth(width),
+        mHeight(height),
+        mChannels(channels),
+        mMinAngle(minAngleDegrees * M_PI / 180.0),
+        mMaxAngle(maxAngleDegrees * M_PI / 180.0),
+        mRand(seed),
+        mTesting(false)
+    {
+        // Do nothing
+    }
+
+private:
+
+    // Rotates a single image (one row in x) by the given angle in radians.
+    void evalSingle(const T* x, const double angle, T* y)
+    {
+        const T* src = x;
+        T* dest      = y;
+
+        double cosAngle = std::cos(-angle);
+        double sinAngle = std::sin(-angle);
+
+        for (size_t channel = 0; channel < mChannels; ++channel)
+        {
+            for (size_t y = 0; y < mHeight; ++y)
+            {
+                double ySinAngle = y * sinAngle;
+                double yCosAngle = y * cosAngle;
+
+                for (size_t x = 0; x < mWidth; ++x)
+                {
+                    // Rotate (x,y) by -angle
+                    double xp = x * cosAngle - ySinAngle;
+                    double yp = x * sinAngle + yCosAngle;
+
+                    // Interpolate via nearest neighbor
+                    int srcX  = std::round(xp);
+                    int srcY  = std::round(yp);
+
+                    // Copy the appropriate pixel
+                    if (srcX < 0 || srcX >= mWidth || srcY < 0 || srcY >= mHeight)
+                        *dest++ = T{};
+                    else *dest++ = src[srcY * mWidth + srcX];
+                }
+            }
+
+            // Advance to the next chanel
+            src += mWidth * mHeight;
+        }
+    }
+
+public:
+
+    void forwardSingle(const T* x, T* y)
+    {
+        if (!mTesting)
+        {
+            // Choose the angle randomly
+            double angle = mRand.nextReal(mMinAngle, mMaxAngle);
+            evalSingle(x, angle, y);
+        }
+
+        else
+        {
+            // Use no rotation for testing. Just pass the data through unaltered.
+            vCopy(x, y, mWidth * mHeight);
+        }
+    }
+
+    void forwardBatch(const Matrix<T>& x, Matrix<T>& y)
+    {
+        if (!mTesting)
+        {
+            // Each sample has a different rotation
+            for (size_t i = 0; i < x.getRows(); ++i)
+            {
+                // Choose the angle randomly
+                double angle = mRand.nextReal(mMinAngle, mMaxAngle);
+                evalSingle(x(i), angle, y(i));
+            }
+        }
+
+        else
+        {
+            // Use no rotation for testing. Just pass the data through unaltered.
+            vCopy(x.data(), y.data(), x.getRows() * x.getCols());
+        }
+    }
+
+    std::string getName() const override
+    {
+        return "Random Rotation Layer";
+    }
+
+    std::string* getProperties(size_t& numElements) const override
+    {
+        const size_t NUM_ELEMENTS = 2;
+        std::string* arr = new std::string[NUM_ELEMENTS];
+
+        char buffer[1024];
+        snprintf(buffer, 1024, "(%zux%zux%zu)",
+            mWidth, mHeight, mChannels);
+        arr[0] = string(buffer);
+
+        snprintf(buffer, 1024, "R: [%.2f, %.2f]", mMinAngle, mMaxAngle);
+        arr[1] = string(buffer);
+
+        numElements = NUM_ELEMENTS;
+        return arr;
+    }
+
+    void setTesting(bool testing)
+    {
+        mTesting = testing;
+    }
+
+    // Simple getters
+    size_t getWidth()    const { return mWidth;                   }
+    size_t getHeight()   const { return mHeight;                  }
+    size_t getChannels() const { return mChannels;                }
+    double getMinAngle() const { return mMinAngle * 180.0 / M_PI; }
+    double getMaxAngle() const { return mMaxAngle * 180.0 / M_PI; }
+
+private:
+    size_t mWidth, mHeight, mChannels;
+    double mMinAngle, mMaxAngle;
+
+    Rand mRand;
+    bool mTesting;
+};
 
 }
 #endif
